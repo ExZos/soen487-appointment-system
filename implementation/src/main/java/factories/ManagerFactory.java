30e758cfb30fe129e311f96acb9099de9fc9f0f0
Timothy <exzos_graalonline@hotmail.com>
2021-03-25 15:15:36 -0400

    Setup modules

---
+++ implementation/src/main/java/factories/ManagerFactory.java
@@ -0,0 +1,27 @@
+package factories;
+
+import repository.interfaces.IManager;
+import utilities.ConfigReader;
+
+import java.lang.reflect.Constructor;
+
+public enum ManagerFactory {
+    TEMP("TempManagerImpl");
+
+    private IManager manager;
+
+    ManagerFactory(String configKey) {
+        try {
+            Class<?> cl = Class.forName(ConfigReader.getConfigFileKey(ConfigReader.CONFIG_FILE, configKey));
+            Constructor<?> cons = cl.getConstructor();
+            manager = (IManager) cons.newInstance();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public IManager getManager(){
+        return manager;
+    }
+}
