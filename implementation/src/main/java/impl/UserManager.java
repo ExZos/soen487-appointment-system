61c06ea0a50f82b73a5107b28ab381276bd93545
Timothy <exzos_graalonline@hotmail.com>
2021-03-26 23:27:02 -0400

    Undeleted code used to test

--- implementation/src/main/java/impl/UserManager.java
+++ implementation/src/main/java/impl/UserManager.java
@@ -32,4 +32,20 @@ public class UserManager implements IUserManager {
 
         return UserDAO.selectUserById(id);
     }
+
+    // QUICK TESTING
+    // BTW on IntelliJ, "ctrl + /" is a comment/uncomment key combination
+    // So try highlighting multiple lines and press "ctrl + /"
+//    public static void main(String[] args) {
+//        try {
+//            UserManager userManager = new UserManager();
+//
+//            System.out.println(userManager.createUser("email@email.com", null));
+//            System.out.println(userManager.getUserById(1));
+//            System.out.println(userManager.updateUserToken(1, "FAKETOKEN"));
+//            System.out.println(userManager.getUserByEmail("email@email.com"));
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
