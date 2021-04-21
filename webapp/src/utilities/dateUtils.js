d6a5d883ef8925dccd35257aa10fced838808c46
Timothy <exzos_graalonline@hotmail.com>
2021-04-21 16:27:33 -0400

    Frontend appt details month fix

--- webapp/src/utilities/dateUtils.js
+++ webapp/src/utilities/dateUtils.js
@@ -1,16 +1,16 @@
 export const monthString = {
-    1: 'January',
-    2: 'February',
-    3: 'March',
-    4: 'April',
-    5: 'May',
-    6: 'June',
-    7: 'July',
-    8: 'August',
-    9: 'September',
-    10: 'October',
-    11: 'November',
-    12: 'December'
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
 };
 
 export const dayString = {
@@ -36,6 +36,8 @@ export const dateFormatter = {
         const month = date.getMonth();
         const dayOfMonth = date.getDate();
         const dayOfWeek = date.getDay();
+
+        console.log(month);
 
         return dayString[dayOfWeek] + ', ' + monthString[month] + ' ' + dayOfMonth + ', ' + year;
     }
